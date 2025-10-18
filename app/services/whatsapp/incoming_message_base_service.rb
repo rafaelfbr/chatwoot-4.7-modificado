90fa855d1466012f9f6d32d22c6d851a96ce36d1
rafaelfbr <52291522+rafaelfbr@users.noreply.github.com>
2025-10-18 14:09:58 -0300

    Add files via upload
    whatsapp referral

--- app/services/whatsapp/incoming_message_base_service.rb
+++ app/services/whatsapp/incoming_message_base_service.rb
@@ -5,6 +5,8 @@ class Whatsapp::IncomingMessageBaseService
   include ::Whatsapp::IncomingMessageServiceHelpers
 
   pattr_initialize [:inbox!, :params!]
+  
+  attr_accessor :referral_data
 
   def perform
     processed_params
@@ -59,6 +61,8 @@ class Whatsapp::IncomingMessageBaseService
   def create_messages
     message = @processed_params[:messages].first
     log_error(message) && return if error_webhook_event?(message)
+
+    @referral_data = message[:referral]
 
     process_in_reply_to(message)
 
--- app/services/whatsapp/incoming_message_service.rb
+++ app/services/whatsapp/incoming_message_service.rb
@@ -3,33 +3,17 @@
 
 class Whatsapp::IncomingMessageService < Whatsapp::IncomingMessageBaseService
 
-  # O método abaixo sobrescreve o método original do arquivo "pai"
-  # (incoming_message_base_service.rb)
-  #
-  # Nós o copiamos e adicionamos a lógica da Issue #12560
-  # para capturar os dados de 'referral' e 'url_preview'.
-
   private
 
   def create_regular_message(message)
-    # --- INÍCIO DA MODIFICAÇÃO DA ISSUE #12560 ---
-
-    # 1. Extrair os dados de 'referral' e 'url_preview' do objeto 'message'
-    url_preview = message[:url_preview]
-    referral = message[:referral]
-
-    # 2. Preparar o hash de 'content_attributes' (só o criamos se os dados existirem)
     content_attrs = {}
-    if url_preview.present? || referral.present?
+    
+    if @referral_data.present?
       content_attrs[:whatsapp] = {
-        url_preview: url_preview,
-        referral: referral
+        referral: @referral_data
       }
     end
-    # --- FIM DA MODIFICAÇÃO DA ISSUE #12560 ---
 
-    # 3. Código copiado do método 'create_message(message)' do arquivo "pai"
-    #    Tivemos que copiá-lo para poder adicionar a nova linha 'content_attributes'
     @message = @conversation.messages.build(
       content: message_content(message),
       account_id: @inbox.account_id,
@@ -38,15 +22,12 @@ class Whatsapp::IncomingMessageService < Whatsapp::IncomingMessageBaseService
       sender: @contact,
       source_id: message[:id].to_s,
       in_reply_to_external_id: @in_reply_to_external_id,
-
-      # 4. ADIÇÃO DA NOVA LINHA:
-      #    Aqui é onde salvamos os dados do anúncio no banco de dados.
       content_attributes: content_attrs
     )
 
-    # 5. Restante do código original de 'create_regular_message' do arquivo "pai"
     attach_files
     attach_location if message_type == 'location'
     @message.save!
   end
+
 end
